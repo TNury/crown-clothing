8ccd6cba60ac003fec90f7b9cd7c3db38436ddd1
Yuri Souza <yurdesou@gmail.com>
2021-08-03 17:53:24 -0300

    converted inventory-overview styles from scss to jss

--- src/App/Components/page-components/shop/inventory-overview/inventory-overview.jsx
+++ src/App/Components/page-components/shop/inventory-overview/inventory-overview.jsx
@@ -3,11 +3,15 @@ import { connect } from 'react-redux';
 import { inventoryPreviewSelector } from '../../../../Redux/reducers/inventory/selectors/inventorySelectors.js';
 // COMPONENTS
 import { InventoryPreview } from './inventory-preview/inventory-preview.jsx';
+// STYLES
+import { collectionsOverviewStyles } from './inventory-overview.styles.js';
 
 const InventoryOverview = ({ reduxProps: { inventoryItemProps } }) => {
 
+  const { collectionsOverview } = collectionsOverviewStyles();
+
   return (
-    <div className="collections-overview">
+    <div className={collectionsOverview}>
       {inventoryItemProps.map(({ id, ...restOfProps }) => (
         <InventoryPreview key={id} {...restOfProps} />
       ))}
---
+++ src/App/Components/page-components/shop/inventory-overview/inventory-overview.styles.js
@@ -0,0 +1,9 @@
+// STYLES CREATOR FUNCTION
+import { createUseStyles } from 'react-jss';
+
+export const collectionsOverviewStyles = createUseStyles({
+  collectionsOverview: {
+    display: 'flex',
+    flexDirection: 'column'
+  }
+});
