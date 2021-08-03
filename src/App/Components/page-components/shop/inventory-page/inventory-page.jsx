54787404e565226d39b17deb9ae8feb247d52268
Yuri Souza <yurdesou@gmail.com>
2021-08-03 17:47:09 -0300

    converted inventory-page styles from scss to jss

--- src/App/Components/page-components/shop/inventory-page/inventory-page.jsx
+++ src/App/Components/page-components/shop/inventory-page/inventory-page.jsx
@@ -1,24 +1,24 @@
 // REDUX
 import { connect } from 'react-redux';
 import { categorySelector } from '../../../../Redux/reducers/inventory/selectors/inventorySelectors.js';
-
 // COMPONENTS
 import InventoryItem from '../../../reusable-components/inventory-item/inventory-item.jsx';
+// STYLES
+import { inventoryPageStyles } from './inventory-page.styles.js';
 
 // This is technically the Category page
 const InventoryPage = ({ reduxProps: { categoryProps } }) => {
 
   const { title: category, items } = categoryProps;
+  const { collectionPage, title, itemsContainer } = inventoryPageStyles();
 
   return (
-    <div className="collection-page">
-      <h2 className="title">{category}</h2>
-      <div className="items">
-        {
-          items.map((item) => (
-            <InventoryItem key={item.id} drilledProps={item}/>
-          ))
-        }
+    <div className={collectionPage}>
+      <h2 className={title}>{category}</h2>
+      <div className={itemsContainer}>
+        {items.map((item) => (
+          <InventoryItem key={item.id} drilledProps={item} />
+        ))}
       </div>
     </div>
   );
---
+++ src/App/Components/page-components/shop/inventory-page/inventory-page.styles.js
@@ -0,0 +1,19 @@
+// STYLES CREATOR FUNCTION
+import { createUseStyles } from 'react-jss';
+
+export const inventoryPageStyles = createUseStyles({
+  collectionPage: {
+    display: 'flex',
+    flexDirection: 'column'
+  },
+  title: {
+    fontSize: '38px',
+    margin: '0 auto 30px',
+    textTransform: 'uppercase'
+  },
+  itemsContainer: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr 1fr 1fr',
+    gridGap: '10px'
+  }
+});
