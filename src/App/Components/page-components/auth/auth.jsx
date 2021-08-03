884ecbe209645473664d74dc593e1277c15a01c0
Yuri Souza <yurdesou@gmail.com>
2021-08-03 18:10:23 -0300

    converted auth-page styles from scss to jss

--- src/App/Components/page-components/auth/auth.jsx
+++ src/App/Components/page-components/auth/auth.jsx
@@ -1,11 +1,15 @@
 // COMPONENTS
 import { SignIn } from './sign-in/sign-in.jsx';
 import { SignUp } from './sign-up/sign-up.jsx';
+// STYLES
+import { authPageStyles } from './auth.styles.js';
 
 export const AuthPage = () => {
 
+  const { authPage } = authPageStyles();
+
   return (
-    <div className="auth-page">
+    <div className={authPage}>
       <SignIn />
       <SignUp />
     </div>
---
+++ src/App/Components/page-components/auth/auth.styles.js
@@ -0,0 +1,11 @@
+// STYLES CREATOR FUNCTION
+import { createUseStyles } from 'react-jss';
+
+export const authPageStyles = createUseStyles({
+  authPage: {
+    width: '850px',
+    display: 'flex',
+    justifyContent: 'space-between',
+    margin: '30px auto'
+  }
+});
