5bd478ff3cf9aba283f5d563b4c37bbcbd3723f9
Yuri Souza <yurdesou@gmail.com>
2021-08-03 17:30:12 -0300

    converted directory menu styles from scss to jss

--- src/App/Components/page-components/home/directory-menu/directory-menu.jsx
+++ src/App/Components/page-components/home/directory-menu/directory-menu.jsx
@@ -5,11 +5,15 @@ import { connect } from 'react-redux';
 import { directorySectionSelector } from '../../../../Redux/reducers/directory/selectors/directorySelectors.js';
 // COMPONENTS
 import MenuItem from './menu-item/menu-item.jsx';
+// STYLES
+import { directoryMenuStyles } from './directory-menu.styles.js';
 
 export const DirectoryMenu = ({ reduxProps: { directoryProps } }) => {
 
+  const { directoryMenu } = directoryMenuStyles();
+
   return (
-    <div className="directory-menu">
+    <div className={directoryMenu}>
       {directoryProps.map(({ id, ...restOfProps }) => (
         <MenuItem key={id} {...restOfProps} />
       ))}
---
+++ src/App/Components/page-components/home/directory-menu/directory-menu.styles.js
@@ -0,0 +1,11 @@
+// STYLES CREATOR FUNCTION
+import { createUseStyles } from 'react-jss';
+
+export const directoryMenuStyles = createUseStyles({
+  directoryMenu: {
+    width: '100%',
+    display: 'flex',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between'
+  }
+});
