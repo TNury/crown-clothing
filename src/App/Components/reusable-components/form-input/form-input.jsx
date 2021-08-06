57e6f67cef36a0b3dd9a48db32e1aab9d7c42880
Yuri Souza <yurdesou@gmail.com>
2021-08-06 18:37:55 -0300

    converted form-input styles from scss to css

--- src/App/Components/reusable-components/form-input/form-input.jsx
+++ src/App/Components/reusable-components/form-input/form-input.jsx
@@ -1,14 +1,23 @@
-
+// STYLES
+import { formInputStyles } from './form-input.styles.js';
 
 export const FormInput = ({ handler, labelText, ...restOfProps }) => {
 
+  const {
+    group,
+    formInput,
+    input,
+    formInputLabel,
+    _shrink
+  } = formInputStyles();
+
   return (
-    <div className="group">
-      <input onChange={handler} {...restOfProps} className="form-input" />
+    <div className={group}>
+      <input className={`${formInput} ${input}`} onChange={handler} {...restOfProps}  />
       {
         labelText
         ?
-        <label className={`form-input-label ${ restOfProps.value.length ? '_shrink' : '_default'}`}>
+        <label className={`${formInputLabel} ${ restOfProps.value.length ? _shrink : '_default'}`}>
           { labelText }
         </label>
         :
---
+++ src/App/Components/reusable-components/form-input/form-input.styles.js
@@ -0,0 +1,52 @@
+// STYLES CREATOR FUNCTION
+import { createUseStyles } from 'react-jss';
+
+const subColor= 'grey';
+const mainColor = 'black';
+
+const shrinkLabel = () => ({
+  top: '-14px',
+  fontSize: '12px',
+  color: mainColor
+});
+
+export const formInputStyles = createUseStyles({
+  group: {
+    position: 'relative',
+    margin: '45px 0'
+  },
+  formInput: {
+    background: 'none',
+    backgroundColor: 'white',
+    color: `${subColor}`,
+    fontSize: '18px',
+    padding: '10px 10px 10px 5px',
+    display: 'block',
+    width: '100%',
+    border: 'none',
+    borderRadius: '0',
+    borderBottom: `1px solid ${subColor}`,
+    margin: '25px 0',
+
+    '&:focus': {
+      outline: 'none'
+    },
+    '&:focus $formInputLabel': shrinkLabel(),
+  },
+  input: {
+    "&[type='password']": {
+      letterSpacing: '0.3em'
+    }
+  },
+  formInputLabel: {
+    color: '$sub-color',
+    fontSize: '16px',
+    fontWeight: 'normal',
+    position: 'absolute',
+    pointerEvents: 'none',
+    left: '5px',
+    top: '10px',
+    transition: '300ms ease all'
+  },
+  _shrink: shrinkLabel()
+});
