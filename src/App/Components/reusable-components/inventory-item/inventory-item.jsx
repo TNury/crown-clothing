07e032338b0f5557398f63c2d37cbb9ae9ae62a4
Yuri Souza <yurdesou@gmail.com>
2021-08-06 16:49:13 -0300

    converted inventory-item styles from scss to jss

--- src/App/Components/reusable-components/inventory-item/inventory-item.jsx
+++ src/App/Components/reusable-components/inventory-item/inventory-item.jsx
@@ -3,26 +3,36 @@ import { connect } from 'react-redux';
 import { addItem } from '../../../Redux/reducers/cart/actions/cartActions.js';
 // REUSABLE COMPONENTS
 import { Button } from '../button/button.jsx';
+// STYLES
+import { inventoryItemStyles } from './inventory-item.styles.js';
 
 const InventoryItem = ({ drilledProps, dispatch }) => {
 
   const { name, price, imageUrl } = drilledProps;
+  const {
+    collectionItem,
+    image: imageStyles,
+    customButton,
+    collectionFooter,
+    name: nameStyles,
+    price: priceStyles
+  } = inventoryItemStyles();
 
   return (
-    <div className="collection-item">
+    <div className={collectionItem}>
       <div
-        className="image"
+        className={imageStyles}
         style={{
           background: `url(${imageUrl})`
         }}
       />
-      <div className="collection-footer">
-        <span className="name">{name}</span>
-        <span className="price">${price}</span>
+      <div className={collectionFooter}>
+        <span className={nameStyles}>{name}</span>
+        <span className={priceStyles}>${price}</span>
       </div>
       <Button
         handler={() => dispatch(addItem(drilledProps))}
-        modifier="_inverted"
+        styles={customButton}
         type="button"
         text="ADD TO CART"
       />
---
+++ src/App/Components/reusable-components/inventory-item/inventory-item.styles.js
@@ -0,0 +1,56 @@
+// STYLES CREATOR FUNCTION
+import { createUseStyles } from 'react-jss';
+
+export const inventoryItemStyles = createUseStyles({
+  collectionItem: {
+    width: '22vw',
+    display: 'flex',
+    flexDirection: 'column',
+    margin: '20px 0',
+    height: '350px',
+    alignItems: 'center',
+    position: 'relative',
+    '&:hover $image': {
+      opacity: '0.8'
+    },
+    '&:hover $customButton': {
+      opacity: '0.85',
+      display: 'flex'
+    }
+  },
+  image: {
+    width: '100%',
+    height: '95%',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    marginBottom: '5px'
+  },
+  customButton: {
+    width: '80%',
+    opacity: '0.7',
+    position: 'absolute',
+    top: '255px',
+    display: 'none',
+    color: 'black',
+    backgroundColor: 'white',
+
+    '&:hover': {
+      color: 'white',
+      backgroundColor: 'black'
+    }
+  },
+  collectionFooter: {
+    width: '100%',
+    height: '5%',
+    display: 'flex',
+    justifyContent: 'space-between',
+    fontSize: '18px'
+  },
+  name: {
+    width: '90%',
+    marginBottom: '15px'
+  },
+  price: {
+    width: '10%'
+  }
+});
