5aa2f4082a42d8cc204efc5da12e77e05665b5d3
Yuri Souza <yurdesou@gmail.com>
2021-08-06 18:10:36 -0300

    converted menu-item styles from scss to css

--- src/App/Components/page-components/home/directory-menu/menu-item/menu-item.jsx
+++ src/App/Components/page-components/home/directory-menu/menu-item/menu-item.jsx
@@ -1,22 +1,34 @@
 // REACT-ROUTER
 import { withRouter } from 'react-router-dom';
+// STYLES
+import { menuItemStyles } from './menu-item.styles.js';
 
-const MenuItem = ({ title, imageUrl, modifier, linkUrl, history, match }) => {
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => {
   
+  const imageSize = size === '_large' ? '380px' : '240px';
+
+  const { 
+    menuItem, 
+    backgroundImage, 
+    content, 
+    title: titleStyles, 
+    subtitle 
+  } = menuItemStyles({size: imageSize});
+
   return (
     <div
-      className={`menu-item ${modifier ? modifier : '_default'}`}
+      className={menuItem}
       onClick={() => history.push(`${match.url}${linkUrl}`)}
     >
       <div
-        className="background-image"
+        className={backgroundImage}
         style={{
           backgroundImage: `url(${imageUrl})`,
         }}
       />
-      <div className="content">
-        <div className="title">{title}</div>
-        <span className="subtitle">SHOP NOW</span>
+      <div className={content}>
+        <div className={titleStyles}>{title}</div>
+        <span className={subtitle}>SHOP NOW</span>
       </div>
     </div>
   );
---
+++ src/App/Components/page-components/home/directory-menu/menu-item/menu-item.styles.js
@@ -0,0 +1,61 @@
+// STYLES CREATOR FUNCTION
+import { createUseStyles } from 'react-jss';
+
+export const menuItemStyles = createUseStyles({
+  menuItem: {
+    minWidth: '30%',
+    height: (props) => props.size,
+    flex: '1 1 auto',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    border: '1px solid black',
+    margin: '0 7.5px 15px',
+    overflow: 'hidden',
+    '&:first-child': {
+      marginRight: '7.5px'
+    },
+    '&:last-child': {
+      marginLeft: '2.5px'
+    },
+    '&:hover': {
+      cursor: 'pointer'
+    },
+    '&:hover $backgroundImage': {
+      transform: 'scale(1.1)',
+      transition: 'transform 6s cubic-bezier(0.25, 0.45, 0.45, 0.95)'
+    },
+    '&:hover $content': {
+      opacity: '0.9'
+    }
+  },
+  backgroundImage: {
+    width: '100%',
+    height: '100%',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center'
+  },
+  content: {
+    height: '90px',
+    padding: '0 25px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    border: '1px solid black',
+    backgroundColor: 'white',
+    opacity: '0.7',
+    position: 'absolute'
+  },
+  title: {
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+    margin: '0 6px 0',
+    fontSize: '22px',
+    color: '#4a4a4a'
+  },
+  subtitle: {
+    fontWeight: 'lighter',
+    fontSize: '16px'
+  }
+});
