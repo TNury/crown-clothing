9ec50847c6914526a37e2338c4aac0ee903f866a
Yuri Souza <yurdesou@gmail.com>
2021-08-12 16:49:26 -0300

    finished implementing loading screen HOC, fixing the bugs introduced 3 commits ago

--- src/App/Components/page-components/shop/shop.jsx
+++ src/App/Components/page-components/shop/shop.jsx
@@ -10,11 +10,20 @@ import {
   firestore,
   convertCollectionSnapshotToMap
 } from '../../../Firebase/firebase.js';
+// HIGHER ORDER COMPONENTS
+import { WithSpinner } from '../../reusable-components/with-spinner/withSpinner.jsx';
 // COMPONENTS
 import InventoryOverview from './inventory-overview/inventory-overview.jsx';
 import InventoryPage from './inventory-page/inventory-page.jsx';
 
+// COMPONENTS WITH LOADING SCREEN HOC
+const InventoryOverviewWithSpinner = WithSpinner(InventoryOverview);
+const InventoryPageWithSpinner = WithSpinner(InventoryPage);
+
 class ShopPage extends React.Component {
+  state = {
+    loading: true
+  };
 
   unsubscribeFromSnapshot = null;
 
@@ -29,6 +38,8 @@ class ShopPage extends React.Component {
       
       dispatch(updateInventory(inventoryMap));
 
+      this.setState({ loading: false });
+
     });
 
   }
@@ -36,11 +47,27 @@ class ShopPage extends React.Component {
   render() {
 
     const { match } = this.props;
+    const { loading: loadingStatus } = this.state;
 
     return (
       <div className="shop-page">
-        <Route exact path={`${match.path}`} component={InventoryOverview} />
-        <Route path={`${match.path}/:categoryId`} component={InventoryPage} />
+        <Route 
+          exact 
+          path={`${match.path}`} 
+          render={(props) => { 
+            return (
+              <InventoryOverviewWithSpinner isLoading={loadingStatus} {...props} />
+            );
+          }}    
+        />
+        <Route 
+          path={`${match.path}/:categoryId`} 
+          render={(props) => {
+            return (
+              <InventoryPageWithSpinner isLoading={loadingStatus} {...props} />
+            );
+          }}
+        />
       </div>
     )
   }
